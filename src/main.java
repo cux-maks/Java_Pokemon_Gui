97bedb2126a98d04ad920aa2590d0d1d10a21fc3
cux-maks <volcano020716@gmail.com>
2022-08-09 22:43:55 +0900

    20220809_4

--- src/main.java
+++ src/main.java
@@ -1,10 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
 
-
-public class main {
+public class main extends JFrame {
+	
+	public main() {
+		setTitle("포켓몬스터 대전");
+		setSize(1200, 900);
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
 		
-		
+		main frame = new main();
 
 	}
 
