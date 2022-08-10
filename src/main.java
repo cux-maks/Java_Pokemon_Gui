79c5b05e3bf514a3b056f87d7bc0ad50add93774
cux-maks <volcano020716@gmail.com>
2022-08-10 16:21:32 +0900

    20220810_1

--- src/main.java
+++ src/main.java
@@ -7,8 +7,54 @@ public class main extends JFrame {
 	
 	public main() {
 		setTitle("포켓몬스터 대전");
+		createMenu();
 		setSize(1200, 900);
 		setVisible(true);
+	}
+	
+	public void createMenu() {
+		
+		JMenuBar mb = new JMenuBar();
+		JMenu screenMenu = new JMenu("메뉴");
+		JMenuItem [] menuItem = new JMenuItem[6];
+		String[] menuName = {"메인 화면", "훈련", "내 포켓몬", "내 기술", "포켓몬 도감", "기술 도감"};
+		MenuActionListener listener = new MenuActionListener();
+		
+		for(int i = 0; i < menuItem.length; i++) {
+			menuItem[i] = new JMenuItem(menuName[i]);
+			menuItem[i].addActionListener(listener);
+			screenMenu.add(menuItem[i]);
+		}
+		 
+		mb.add(screenMenu);
+		setJMenuBar(mb);
+		
+	}
+	
+	class MenuActionListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String cmd = e.getActionCommand();
+			switch (cmd) {
+			case "메인 화면":
+				System.out.println(cmd);
+				break;
+			case "훈련":
+				System.out.println(cmd);
+				break;
+			case "내 포켓몬": 
+				System.out.println(cmd);
+				break;
+			case "내 기술":
+				System.out.println(cmd);
+				break;
+			case "포켓몬 도감":
+				System.out.println(cmd);
+				break;
+			case "기술 도감":
+				System.out.println(cmd);
+				break;
+			}
+		}
 	}
 
 	public static void main(String[] args) {
--- src/pokemon/Charmander.java
+++ src/pokemon/Charmander.java
@@ -2,4 +2,20 @@ package pokemon;
 
 public class Charmander extends monster {
 
+	public Charmander() {
+		int Munchkin =  (int)((Math.random()*10000)%100);
+		if(Munchkin == 1) {
+			super.Set_Hp(3000);
+			super.Set_Atk(99999);
+			super.Set_Dfs(99999);
+			super.Set_S_Atk(99999);
+			super.Set_S_Dfs(99999);
+			super.Set_Critical(100);
+			super.Set_Accuracy(100);
+			super.Set_Evasion(100);
+			super.Set_Speed(3000);
+			
+		}
+	}
+	
 }
